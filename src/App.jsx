9b9b94921be300fd9733d90175bc6f8fb31c0647
Kamron5505 <kama58077@gmail.com>
2025-09-30 16:46:00 +0500

    sphers finished

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
     <>
       <Header />
       <Hero />
-      {/* <Sphers /> */}
+      <Sphers />
       {/* <Achievements /> */}
       {/* <Portfolio /> */}
       {/* <Stek /> */}
